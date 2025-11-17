ea3b7e2790fb1962f4e1271ce010ac4e7732f06d
goncoli <gonzalojose.colina@estudiantes.uva.es>
2025-11-17 12:13:50 +0100

    feat: ajustar la altura de la barra de navegación según la orientación de la pantalla

--- app/src/main/java/com/example/mariamolina/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/mariamolina/ui/navigation/AppNavigation.kt
@@ -1,6 +1,9 @@
 package com.example.mariamolina.ui.navigation
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -19,9 +22,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
@@ -44,6 +50,9 @@ import com.example.mariamolina.R
 fun AppNavigation() {
     val navControllerPrincipal = rememberNavController()
 
+    val configuration = LocalConfiguration.current
+    val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+
     // 1. Escuchamos al controlador principal (para títulos y barra inferior)
     val navBackStackEntry by navControllerPrincipal.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
@@ -64,7 +73,9 @@ fun AppNavigation() {
             }
         },
         bottomBar = {
-            NavigationBar {
+            NavigationBar(
+                modifier = Modifier.height(if (isLandscape) 50.dp else 80.dp)
+            ) {
                 itemsNavegacion.forEach { pantalla ->
                     val isSelected =
                         currentDestination?.hierarchy?.any { it.route == pantalla.ruta } == true
@@ -80,8 +91,8 @@ fun AppNavigation() {
                                 restoreState = true
                             }
                         },
-                        label = { Text(pantalla.tituloBottomBar) },
-                        icon = { androidx.compose.material3.Icon(pantalla.icono, contentDescription = pantalla.tituloBottomBar) },
+                        label = { if (!isLandscape) Text(pantalla.tituloBottomBar, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center) },
+                        icon = { Icon(pantalla.icono, contentDescription = pantalla.tituloBottomBar) },
                         colors = NavigationBarItemDefaults.colors(
                             selectedIconColor = MaterialTheme.colorScheme.primary,
                             selectedTextColor = MaterialTheme.colorScheme.primary,
