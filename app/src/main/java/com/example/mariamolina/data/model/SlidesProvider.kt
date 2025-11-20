e839054aa4b0a340bddebd8c1eff4a79cd8a5306
goncoli <gonzalojose.colina@estudiantes.uva.es>
2025-11-20 19:29:41 +0100

    feat: links fotos

--- app/src/main/java/com/example/mariamolina/data/model/SlidesProvider.kt
+++ app/src/main/java/com/example/mariamolina/data/model/SlidesProvider.kt
@@ -7,107 +7,106 @@ object SlidesProvider {
             id = "slide1",
             title = "María de Molina",
             description = "¡Hola! Soy María de Molina y te voy a contar mi historia.",
-            imageUrl = "mariademolina_photoroom",
+            imageUrl = "https://ik.imagekit.io/vy6xp2vke/MariaDeMolina-Photoroom.png",
             hasSpeechBubble = true
         ),
         Slide(
             id = "slide2",
             title = "Mis Padres",
             description = "Mis padres fueron el infante Alfonso de Molina y mi madre Doña Mayor de Meneses.",
-            imageUrl = "mayor_de_meneses_photoroom",
+            imageUrl = "https://ik.imagekit.io/vy6xp2vke/padres-maria.PNG",
             hasSpeechBubble = true
         ),
         Slide(
             id = "slide3",
             title = "Castillo de Montealegre",
             description = "Castillo de Montealegre",
-            imageUrl = "escudo_de_castilla_y_leon_sin_corona",
+            imageUrl = "https://ik.imagekit.io/fn2wdosiw/CastilloMontealegre.jpg",
             hasSpeechBubble = false
         ),
         Slide(
             id = "slide4",
             title = "Castillo de Meneses",
             description = "Castillo de Meneses",
-            imageUrl = "escudo_de_castilla_y_leon_sin_corona",
+            imageUrl = "https://ik.imagekit.io/vy6xp2vke/MariaDeMolina-Photoroom.png",
             hasSpeechBubble = false
         ),
         Slide(
             id = "slide5",
             title = "El Príncipe Sancho",
             description = "Cuando tenía 15 años, un príncipe llamado Sancho se enamoró de mí.",
-            imageUrl = "sancho_principe_photoroom",
+            imageUrl = "https://ik.imagekit.io/vy6xp2vke/SanchoPrincipe-Photoroom.png",
             hasSpeechBubble = true
         ),
         Slide(
             id = "slide6",
             title = "Nuestra Boda",
             description = "En 1282 nos casamos en Toledo.",
-            imageUrl = "maria_adolescente",
+            imageUrl = "https://ik.imagekit.io/vy6xp2vke/maria-sancho-boda.PNG",
             hasSpeechBubble = true
         ),
         Slide(
             id = "slide7",
             title = "Palacio de Valladolid",
             description = "Después nos fuimos a vivir a Valladolid al palacio de Sancho.",
-            imageUrl = "mariademolina_photoroom",
+            imageUrl = "https://ik.imagekit.io/vy6xp2vke/MariaAdolescente-Photoroom.png",
             hasSpeechBubble = true
         ),
         Slide(
             id = "slide8",
             title = "Reyes de Castilla y León",
             description = "Cuando murió el padre de Sancho, que era rey, eligieron a Sancho como Rey de Castilla y León. En ese momento yo me convertí en Reina de Castilla y León.",
-            imageUrl = "sancho_rey_photoroom",
+            imageUrl = "https://ik.imagekit.io/vy6xp2vke/SanchoRey-Photoroom.png",
             hasSpeechBubble = true
         ),
         Slide(
             id = "slide9",
             title = "Las Batallas",
             description = "Durante el reinado, ayudé a Sancho a pelear en muchas batallas.",
-            imageUrl = "maria_molina_batalla_photoroom",
+            imageUrl = "https://ik.imagekit.io/vy6xp2vke/maria-sancho-boda.PNG",
             hasSpeechBubble = true
         ),
         Slide(
             id = "slide10",
             title = "El Rey Fernando",
             description = "Años más tarde, Sancho se puso enfermo y murió. Entonces conseguí que nombraran rey a mi hijo Fernando.",
-            imageUrl = "maria_molina_pie_photoroom",
+            imageUrl = "https://ik.imagekit.io/vy6xp2vke/MariaMolina_pie-Photoroom.png",
             hasSpeechBubble = true
         ),
         Slide(
             id = "slide11",
             title = "Regente de Fernando",
             description = "Fernando todavía era pequeño y le ayudé a reinar hasta ser mayor de edad.",
-            imageUrl = "maria_molina_bebe_photoroom",
+            imageUrl = "https://ik.imagekit.io/vy6xp2vke/MariaDeMolina-Photoroom.png",
             hasSpeechBubble = true
         ),
         Slide(
             id = "slide12",
             title = "El Rey Alfonso",
             description = "Fernando fue muy buen rey, pero también se puso enfermo y murió. Como su hijo Alfonso (mi nieto) era muy pequeño, le ayudé a reinar.",
-            imageUrl = "escudo_alfonso",
+            imageUrl = "https://ik.imagekit.io/vy6xp2vke/MariaDeMolina-Photoroom.png",
             hasSpeechBubble = true
         ),
         Slide(
             id = "slide13",
             title = "Mi Descanso Final",
             description = "Antes de morir pedí que me enterraran en el monasterio de Santa María La Real de Huelgas.",
-            imageUrl = "mariademolina_photoroom",
+            imageUrl = "https://ik.imagekit.io/vy6xp2vke/MariaDeMolina-Photoroom.png",
             hasSpeechBubble = true
         ),
         Slide(
             id = "slide14",
             title = "El Secreto de mi Tumba",
             description = "Con mi tumba se puede saber si va a llover, ya que unas horas antes aparecen unas manchas encima. Pero ten cuidado… ¡si las tocas pica!",
-            imageUrl = "mariademolina_photoroom",
+            imageUrl = "https://ik.imagekit.io/vy6xp2vke/MariaDeMolina-Photoroom.png",
             hasSpeechBubble = false
         ),
         Slide(
             id = "slide15",
             title = "Figuras Escondidas",
             description = "Además, en mi tumba están escondidas las figuras de una lagartija, un monje tapándose los oídos y un monstruo. ¡Intenta encontrarlas!",
-            imageUrl = "escudo_de_castilla_y_leon_sin_corona",
+            imageUrl = "https://ik.imagekit.io/vy6xp2vke/MariaDeMolina-Photoroom.png",
             hasSpeechBubble = false
         )
     )
 }
-
